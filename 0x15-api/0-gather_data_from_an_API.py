6a54b091e31d7c036ef7658f8d1d2a4e57c95798
Kamal <kamalmiftah01@gmail.com>
2024-05-20 21:23:12 +0100

    adding 'if __name__ == '__main__'

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,46 +1,48 @@
 #!/usr/bin/python3
 """ Python script that consume a REST api"""
-import requests
-import sys
 
 
-""" Get employer_id from command line arguments """
-employer_id = int(sys.argv[1])
+if __name__ == "__main__":
+    import requests
+    import sys
 
-""" Define the API URLs """
-todos_url = 'https://jsonplaceholder.typicode.com/todos'
-users_url = 'https://jsonplaceholder.typicode.com/users'
+    """ Get employer_id from command line arguments """
+    employer_id = int(sys.argv[1])
 
-""" Make a GET request to fetch todos """
-todos_response = requests.get(todos_url)
-todos = todos_response.json()
+    """ Define the API URLs """
+    todos_url = 'https://jsonplaceholder.typicode.com/todos'
+    users_url = 'https://jsonplaceholder.typicode.com/users'
 
-""" Initialize task counters """
-all_tasks = 0
-done = 0
+    """ Make a GET request to fetch todos """
+    todos_response = requests.get(todos_url)
+    todos = todos_response.json()
 
-""" Iterate through the tasks and count """
-for task in todos:
-    if task['userId'] == employer_id:
-        if task['completed']:
-            done += 1
-        all_tasks += 1
+    """ Initialize task counters """
+    all_tasks = 0
+    done = 0
 
-""" Make a GET request to fetch user information """
-users_response = requests.get(users_url)
-users = users_response.json()
+    """ Iterate through the tasks and count """
+    for task in todos:
+        if task['userId'] == employer_id:
+            if task['completed']:
+                done += 1
+            all_tasks += 1
 
-""" Find the user with the given employer_id """
-name = None
-for user in users:
-    if user['id'] == employer_id:
-        name = user['name']
-        break
+    """ Make a GET request to fetch user information """
+    users_response = requests.get(users_url)
+    users = users_response.json()
 
-""" Print the result """
-print('Employee {} is done with tasks({}/{}):'.format(name, done, all_tasks))
+    """ Find the user with the given employer_id """
+    name = None
+    for user in users:
+        if user['id'] == employer_id:
+            name = user['name']
+            break
 
-""" Print the titles of the completed tasks """
-for task in todos:
-    if task['userId'] == employer_id and task['completed']:
-        print("\t {}".format(task['title']))
+    """ Print the result """
+    print('Employee {} is done with tasks({}/{}):'.format(name, done, all_tasks))
+
+    """ Print the titles of the completed tasks """
+    for task in todos:
+        if task['userId'] == employer_id and task['completed']:
+            print("\t {}".format(task['title']))
