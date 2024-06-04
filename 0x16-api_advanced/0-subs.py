60b73c35eb444c407052003862e675be8f4eb1fe
Kamal <kamalmiftah01@gmail.com>
2024-06-04 18:30:43 +0100

    solving task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """ returns the number of subscribers for a given subreddit. """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/kamal_)"
+        "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     try:
         response = requests.get(url, headers=headers)
