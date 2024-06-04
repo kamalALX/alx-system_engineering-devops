cfa9f53e7a8b24cc077cdc131b7478e3ca55ea7c
Kamal <kamalmiftah01@gmail.com>
2024-06-04 18:34:42 +0100

    solving task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+""" queries the Reddit API """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """ returns the number of subscribers for a given subreddit. """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Morocco"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json().get('data')
+        subscribers = data.get('subscribers')
+        return subscribers
+    except (KeyError, requests.RequestException):
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
